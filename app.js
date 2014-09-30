8de39c481947b5100df75e66deb45e1631deb4cc
Drew <drew.r.wilson@gmail.com>
2014-09-30 10:42:58 -0700

    working on loading the view

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-var Stand = Backbone.Model.extend({url:'stands/'});
+var Stand = Backbone.Model.extend();
 
-var stands = Backbone.Collection.extend({
+var Stands = Backbone.Collection.extend({
+    url: 'http://private-159b-lestands.apiary-mock.com/stands',
     model: Stand,
-    url: '',
-    initialize: function() {
-        this.fetch();
+    initialize: function(init) {
+        this.fetch({success:init});
     }
 });
 
-console.log(stands);
+ExampleView = Backbone.View.extend({
+    initialize: function(){
+            this.render();
+        },
+    render: function(){
+          // Compile the template using underscore
+          var template = _.template( $("#big-numbers-template").html(),  {});
+          // Load the compiled HTML into the Backbone "el"
+          this.$el.html( template );
+      }
+});
+
+var exampleView = new ExampleView({ el: $("#big-numbers-container") });
+
+
+function init () {
+  // var bigNumbersTemplate = _.template( $('#big-numbers').html() );
+  // bigNumbersTemplate(myStands.toJSON());
+
+
+
+
+  console.log(myStands.toJSON());
+}
+
+var myStands = new Stands(init);
