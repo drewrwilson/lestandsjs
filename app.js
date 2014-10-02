d63df2a0d4eae4f322bf6de36be098029846fe05
Peter W <techieshark@gmail.com>
2014-10-02 09:42:14 -0700

    sorts stands list by last updated date (oldest first)

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ var Stands = Backbone.Collection.extend({
     initialize: function() {
         this.fetch({reset: true});
     },
+    comparator: function(m) {
+        // http://stackoverflow.com/questions/9540770/using-underscore-to-sort-a-collection-based-on-date
+        return (new Date(m.get('lastUpdateDate'))).getTime();
+    }
 });
 
 var stands = new Stands();
