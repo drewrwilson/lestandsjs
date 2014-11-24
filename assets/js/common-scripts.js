04965140435a867b0f45012ea3fb8a1700a6c091
Peter W <techieshark@gmail.com>
2014-11-23 21:14:55 -0800

    use jQuery’s toggleClass to simplify code
    instead of checking visibility and applying CSS rules
    via JS, we just set the appropriate css rules and
    then toggle a class.

--- assets/js/common-scripts.js
+++ assets/js/common-scripts.js
@@ -31,25 +31,7 @@ var Script = function () {
     });
 
     $('.fa-bars').click(function () {
-        if ($('#sidebar > ul').is(":visible") === true) {
-            $('#main-content').css({
-                'margin-left': '0px'
-            });
-            $('#sidebar').css({
-                'margin-left': '-210px'
-            });
-            $('#sidebar > ul').hide();
-            $("#container").addClass("sidebar-closed");
-        } else {
-            $('#main-content').css({
-                'margin-left': '210px'
-            });
-            $('#sidebar > ul').show();
-            $('#sidebar').css({
-                'margin-left': '0'
-            });
-            $("#container").removeClass("sidebar-closed");
-        }
+        $("#container").toggleClass("sidebar-closed");
     });
 
 
