17dbefe272b1a42c0730a89a968a22c68d6e3212
Drew <drew.r.wilson@gmail.com>
2014-12-16 22:24:50 -0800

    add an index route to API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var restify = require('restify');  //package for API routes
         pg; //database
 */
 
-var port = process.env.PORT || 80; //to deploy on heroku
+var port = process.env.PORT || 80; //it's required to have this environmental variable set in on deploy on heroku
 
 var server = restify.createServer({
   name: 'lestands',
@@ -159,6 +159,23 @@ server.get('/stands/:id/updates/:updateID', function (req, res, next) {
   res.send(update);
   return next();
 });
+
+// /
+// this is just a route for the index of the API
+server.get('/', function (req, res, next) {
+  var bs = {
+    "this": "",
+    "API": "",
+    "is": "",
+    "just": "",
+    "a": "",
+    "test": ""
+  };
+
+  res.send(bs);
+  return next();
+});
+
 
 server.listen(port, function () {
   console.log('%s listening at url %s', server.name, server.url);
