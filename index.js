763c615231d88f26f5bd6a9af86e155d42a6ebe9
Peter W <techieshark@gmail.com>
2014-12-19 16:15:51 -0800

    fetch stands/:id from db

--- index.js
+++ index.js
@@ -40,7 +40,18 @@ var sendSelectionFirstRow = function(query, params, res) {
 // does nothing. See `sendSelectionFirstRow` for example of how this could be used.
 var sendSelection = function (query, params, res, preProcess) {
 
-  if(typeof(preProcess)==='undefined') preProcess = function(rows) { return rows; };
+  var outputHandler = function (rows) {
+    res.send(preProcess(rows));
+  };
+
+  queryDB(query, params, res, outputHandler);
+};
+
+
+// send the database the given query and send the response to the handler
+var queryDB = function (query, params, res, outputHandler) {
+
+  if(typeof(outputHandler)==='undefined') outputHandler = function(rows) { return rows; };
 
   // get a pg client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
@@ -55,6 +66,7 @@ var sendSelection = function (query, params, res, preProcess) {
       // In this case, if we have successfully received a client (truthy)
       // then it will be removed from the pool.
       done(client);
+
       res.writeHead(500, {'content-type': 'text/plain'});
       res.end('An error occurred');
       return true;
@@ -69,11 +81,14 @@ var sendSelection = function (query, params, res, preProcess) {
       // return the client to the connection pool for other requests to reuse
       done();
 
-      res.send(preProcess(result.rows));
+      outputHandler(result.rows);
 
     });
   });
 };
+
+
+
 
 //Static routes:
 // /stands - show all stands
@@ -106,47 +121,72 @@ var query = 'WITH \
 });
 
 // /stands/:id
-server.get('/stands/:id', function (req, res, next) {
-  var stand = {
-    "id": 1,
-    "name": "16th & Mission",
-    "pic": "stand1.jpg",
-    "description": "Amber's stand outside Herbivore",
-    "dateDeployed": "10/20/2013",
-    "dateRetired": "",
-    "updates" : [
-    {
-      "id": 1,
-      "date": "March 15, 2014",
-      "amountWhenChecked": 15,
-      "amountAdded": 80,
-      "comments": "the sticker is coming off"
-    },
-    {
-      "id": 115,
-      "date": "June 11, 2014",
-      "amountWhenChecked": 45,
-      "amountAdded": 30,
-      "comments": ""
-    },
-    {
-      "id": 135,
-      "date": "April 1, 2008",
-      "amountWhenChecked": 0,
-      "amountAdded": 50,
-      "comments": "Just started"
-    },
-    {
-      "id": 1235,
-      "date": "November 22, 2014",
-      "amountWhenChecked": 0,
-      "amountAdded": 500,
-      "comments": "nooooooo"
-    }
-    ]
+server.get('/stands/:standID', function (req, res, next) {
+
+  // get updates for the given stand
+  // get the given stand
+  // add the updates to the stand
+  // send
+
+  var sendStand = function (updates) {
+
+    var selectStandQuery = '\
+      SELECT stands.id, stands.name, stands.description, stands."geoLat", stands."geoLong", \
+             stands.address1, stands.address2, stands.city, stands.state, stands.zip \
+      FROM stands WHERE id = ($1) LIMIT 1'; // note LIMIT 1 sanity check (we only expect 1 anyway)
+
+    var preProcess = function (rows) {
+      var stand = rows[0];
+      stand.updates = updates;
+      return stand;
+    };
+
+    sendSelection(selectStandQuery, [req.params.standID], res, preProcess);
   };
 
-  res.send(stand);
+  var selectUpdatesQuery = 'SELECT * FROM updates WHERE stand_id = ($1)'
+  queryDB(selectUpdatesQuery, [req.params.standID], res, sendStand);
+
+
+  // sendSelection(query, req.params.standID, res, preProcess);
+
+  // var stand = {
+  //   "id": 1,
+  //   "name": "16th & Mission",
+  //   "description": "Amber's stand outside Herbivore",
+  //   "updates" : [
+  //   {
+  //     "id": 1,
+  //     "date": "March 15, 2014",
+  //     "amountWhenChecked": 15,
+  //     "amountAdded": 80,
+  //     "comments": "the sticker is coming off"
+  //   },
+  //   {
+  //     "id": 115,
+  //     "date": "June 11, 2014",
+  //     "amountWhenChecked": 45,
+  //     "amountAdded": 30,
+  //     "comments": ""
+  //   },
+  //   {
+  //     "id": 135,
+  //     "date": "April 1, 2008",
+  //     "amountWhenChecked": 0,
+  //     "amountAdded": 50,
+  //     "comments": "Just started"
+  //   },
+  //   {
+  //     "id": 1235,
+  //     "date": "November 22, 2014",
+  //     "amountWhenChecked": 0,
+  //     "amountAdded": 500,
+  //     "comments": "nooooooo"
+  //   }
+  //   ]
+  // };
+
+  // res.send(stand);
   return next();
 });
 
