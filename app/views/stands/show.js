30de0482578fe26ea9abcda5cf5b10c31ef3ab26
Peter W <techieshark@gmail.com>
2014-12-27 22:16:24 -0800

    whitespace

--- app/views/stands/show.js
+++ app/views/stands/show.js
@@ -8,9 +8,9 @@ var app = app || {};
   app.StandView = Backbone.View.extend({
       template: Handlebars.compile( $("#single-stand-template").html() ),
       initialize: function(){
-              this.listenTo(this.model, 'reset', this.render);
-              this.render();
-          },
+          this.listenTo(this.model, 'reset', this.render);
+          this.render();
+      },
       render: function(){
           var view = {
             id: this.model.attributes.id,
@@ -44,21 +44,21 @@ var app = app || {};
           //determine how many days since last update
           today = new Date();
           view.daysSinceChecked = Math.floor((today - mostRecentUpdate) / (1000*60*60*24));
-            html = this.template(view);
-            this.$el.html(html)
-        },
-        events: {
-          "click a.delete": "deleteUpdate",
-        },
-        deleteUpdate: function(event) {
-          event.preventDefault();
-          var clicked = $(event.target);
-          var updateID = clicked.data('update-id');
-          var standID = clicked.data('stand-id');
+          html = this.template(view);
+          this.$el.html(html)
+      },
+      events: {
+        "click a.delete": "deleteUpdate",
+      },
+      deleteUpdate: function(event) {
+        event.preventDefault();
+        var clicked = $(event.target);
+        var updateID = clicked.data('update-id');
+        var standID = clicked.data('stand-id');
 
-          //this console.log is a placeholder for where we need to trigger a json request that deletes an update with a given id
-          console.log("deleteUpdate action triggered. Deleting update with id #" + updateID + ', on stand with id #' + standID);
-        }
+        //this console.log is a placeholder for where we need to trigger a json request that deletes an update with a given id
+        console.log("deleteUpdate action triggered. Deleting update with id #" + updateID + ', on stand with id #' + standID);
+      }
   });
 
 })(jQuery);
