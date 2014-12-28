336ee3d77b2937773b4b390b19871146d3c0a27f
Peter W <techieshark@gmail.com>
2014-12-28 03:41:12 -0800

    removing apparently unused code

--- app/views/stands/show.js
+++ app/views/stands/show.js
@@ -17,8 +17,6 @@ var app = app || {};
             stand: this.model.attributes
           }
 
-          window.updates = view.stand.updates;
-
           html = this.template(view);
           this.$el.html(html)
       },
