ecddaaeae1ed04f9661666f84d76e2b724795f8c
Peter W <techieshark@gmail.com>
2014-12-29 16:19:31 -0800

    we want stand to have its own Updates collection
    - but it isn’t working

--- app/models/stand.js
+++ app/models/stand.js
@@ -4,8 +4,13 @@ var app = app || {};
 (function () {
   app.Stand = Backbone.Model.extend({
     // this function returns the number of days since the last update on this model
-    urlRoot: 'https://lestands-api.herokuapp.com/stands',
+    // urlRoot: 'https://lestands-api.herokuapp.com/stands',
+    updates: null,
     initialize: function () {
+    	// Here is where we manage to build the nested url
+	    app.updates = new app.Updates({
+	      standPath: this.url()
+	    });
     }
   });
 
