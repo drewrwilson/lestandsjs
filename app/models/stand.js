e446ec918cc71d330738270b66169d3eb6a4510d
Peter W <techieshark@gmail.com>
2014-12-30 01:16:41 -0800

    remove outdated comment

--- app/models/stand.js
+++ app/models/stand.js
@@ -3,7 +3,6 @@ var app = app || {};
 
 (function () {
   app.Stand = Backbone.Model.extend({
-    // this function returns the number of days since the last update on this model
     urlRoot: 'https://lestands-api.herokuapp.com/stands',
     initialize: function () {
     	// Here is where we manage to build the nested url
