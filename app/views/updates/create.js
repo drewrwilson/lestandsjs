29d9a64ba6304b74224a469b681d917e7f253313
Peter W <techieshark@gmail.com>
2015-01-01 01:43:11 -0800

    only render AddUpdateView after we fetch stand
    - because we need the stand name to include in the view, so
      we’ll need the stand first

--- app/views/updates/create.js
+++ app/views/updates/create.js
@@ -6,15 +6,28 @@ var app = app || {};
   //view for adding an update
   app.AddUpdateView = Backbone.View.extend({
       template: Handlebars.compile( $("#add-update-template").html() ),
-      initialize: function(){
+      initialize: function(options){
+              this.standID = options.standID || null;
               //this.listenTo(this.model, 'reset', this.render);
               this.render();
           },
-      render: function(){
-            html = this.template();
-            this.$el.html(html)
-            $('.default-date-picker').datepicker();
-        }
+      render: function () {
+            updateView = this;
+            app.stands.fetch({success: function () {
+              var stand = app.stands.findWhere({id: updateView.standID});
+
+              view = {
+                standID: stand.id,
+                name: stand.get('name')
+              };
+
+              html = updateView.template(view);
+              updateView.$el.html(html);
+              $('.default-date-picker').datepicker();
+            }});
+        },
+
+
   });
 
 })(jQuery);
