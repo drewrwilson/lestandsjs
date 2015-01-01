87db1f595e19d36e699fdd3e97c6da5c74e1def9
Peter W <techieshark@gmail.com>
2014-12-31 18:32:15 -0800

    generate appropriate url for each Update
    - previously for testing we had hard code this

--- app/models/update.js
+++ app/models/update.js
@@ -3,7 +3,9 @@ var app = app || {};
 
 (function () {
   app.Update = Backbone.Model.extend({
-    urlRoot: 'http://lestands-api.herokuapp.com/stands/1/updates/',
+    url: function () {
+      return API_BASE + "/stands/" + this.standID + "/updates/" + this.id;
+    },
   });
 
 }());
