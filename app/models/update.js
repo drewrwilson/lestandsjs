27e51da595e68c5b75975eab44186096a22d4b49
Peter W <techieshark@gmail.com>
2014-12-31 18:31:20 -0800

    remove old commented code in update.js

--- app/models/update.js
+++ app/models/update.js
@@ -3,19 +3,7 @@ var app = app || {};
 
 (function () {
   app.Update = Backbone.Model.extend({
-    // url: function () {
-    //   // /stands/:stand_id/updates/1
-    //   return this.urlRoot;
-    // },
     urlRoot: 'http://lestands-api.herokuapp.com/stands/1/updates/',
-    // url: function () {
-    //   // /stands/:stand_id/updates/1
-    //   return 'http://lestands-api.herokuapp.com/stands/1/updates/1';
-    // },
-
-    // initialize: function (options) {
-    //   this.urlRoot = options.standPath + '/updates';
-    // }
   });
 
 }());
