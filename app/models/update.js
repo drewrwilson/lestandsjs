d777fc2c3f2d26615dfb799ae7c8e8b6c23ab535
Peter W <techieshark@gmail.com>
2015-01-01 02:47:40 -0800

    improve url() so it works even w/o ID
    - when an Update is new, before it is added to DB, it doesn’t have ID and ID will be undefined
    - note that ‘undefined’ is not a JS reserved word but ‘void 0’ produces an ‘undefined’ value which can be tested against `typeof this.id`.

--- app/models/update.js
+++ app/models/update.js
@@ -4,7 +4,12 @@ var app = app || {};
 (function () {
   app.Update = Backbone.Model.extend({
     url: function () {
-      return API_BASE + "/stands/" + this.standID + "/updates/" + this.id;
+      // URL w/o ID for POSTing a new Update that has no ID yet
+      var url = API_BASE + "/stands/" + this.attributes.standID + "/updates";
+      if (typeof this.id !== typeof void 0) { // an existing Update
+        url = url + "/" + this.id;
+      }
+      return url;
     },
   });
 
