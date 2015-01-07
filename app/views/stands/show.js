3cf7ef4dc4e33463359dfd8c2dc2784f249f430c
Peter W <techieshark@gmail.com>
2015-01-06 22:56:53 -0800

    make updates view a proper subview of Stand view
    - this doc helped a lot:
     http://ianstormtaylor.com/rendering-views-in-backbonejs-isnt-always-simple/

--- app/views/stands/show.js
+++ app/views/stands/show.js
@@ -9,11 +9,22 @@ var app = app || {};
       template: Handlebars.compile( $("#single-stand-template").html() ),
       initialize: function(){
           this.listenTo(this.model, 'change', this.render);
+
+          // set up the subview
+          this.updatesView = new app.UpdatesView({
+              el: $("#updates-list"),
+              collection: this.model.updates,
+            });
+
           this.render();
       },
       render: function(){
           html = this.template(this.model.attributes);
-          this.$el.html(html)
+          this.$el.html(html);
+
+          this.updatesView.setElement(this.$('#updates-list')).render();
+
+          return this;
       }
   });
 
