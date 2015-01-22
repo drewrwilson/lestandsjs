3339771518341898bc19860164209304eb89ed7c
Peter W <techieshark@gmail.com>
2015-01-22 14:59:32 -0800

    navigate to the stand we’re updating

--- app/views/updates/create.js
+++ app/views/updates/create.js
@@ -59,7 +59,7 @@ var app = app || {};
             var model = new app.Update(attributes);
 
             model.save();
-            app.router.show(); // jump back to stand BUG NOT WORKING
+            app.router.navigate('#/stands/' + this.standID);
             return false;
       },
   });
